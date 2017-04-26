7598676dabac454f9a5c282d62a61266ae420f52
spookymushroom <github.spookymushroom@gmail.com>
2017-04-25 17:53:30 -0700

    Added origin and referrer headers, fixing the error 403 when downloading from googlevideo

--- domainfrontedyoutube/__init__.py
+++ domainfrontedyoutube/__init__.py
@@ -3,7 +3,7 @@ import urllib.request as ur
 import urllib.parse as up
 import html.parser
 
-req = ur.Request("https://drive.google.com/watch?v=eQVXIPVRbCU",headers={"Host":"www.youtube.com","User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0"})
+req = ur.Request("https://drive.google.com/get_video_info?video_id=_n56KPJVUQ8",headers={"Host":"www.youtube.com","User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0"})
 res = ur.urlopen(req)
 b = res.read()
 s = b.decode("utf-8")
@@ -15,11 +15,11 @@ class PageParser(html.parser.HTMLParser):
 
 #ALL THE URLS IT SPITS BACK GIVE AN ERROR 403, WILL NEED TO FIX
 
-#j = up.parse_qs(s)
-#j2 = up.parse_qs(j["url_encoded_fmt_stream_map"][0])
-#print(j2["url"])
+j = up.parse_qs(s)
+j2 = up.parse_qs(j["url_encoded_fmt_stream_map"][0])
+print(j2["url"])
 
-#req2 = ur.Request(j2["url"][2],headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0"})
+req2 = ur.Request(j2["url"][2],headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0","Origin":"https://www.youtube.com","Referer":"https://www.youtube.com/watch?v=_n56KPJVUQ8"})
 
+res2 = ur.urlopen(req2)
 
-#res2 = ur.urlopen(req2)
