da52255e6786e2d553327f1cc6274ae5d9723aa9
spookymushroom <github.spookymushroom@gmail.com>
2017-04-26 21:16:12 -0700

    Added interactive prompt for url, then downloads that

--- domainfrontedyoutube/__init__.py
+++ domainfrontedyoutube/__init__.py
@@ -2,24 +2,90 @@
 import urllib.request as ur
 import urllib.parse as up
 import html.parser
+import os
 
-req = ur.Request("https://drive.google.com/get_video_info?video_id=_n56KPJVUQ8",headers={"Host":"www.youtube.com","User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0"})
-res = ur.urlopen(req)
+savedir = "~/Videos"
+
+savedir_full = os.path.expanduser(savedir)
+
+
+globalheaders = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0"}
+headers = {"Host":"www.youtube.com"}
+headers.update(globalheaders)
+
+
+
+def getvideoid(url):
+    parsed_url = up.urlparse(url)
+    if parsed_url.netloc == "youtu.be":
+        return parsed_url.path.strip("/")
+    elif parsed_url.netloc == "youtube.com" or parsed_url.netloc == "www.youtube.com":
+        q = up.parse_qs(parsed_url.query)
+        return q["v"][0]
+
+def getmetaurl(video_id):
+    return "https://www.youtube.com/get_video_info?video_id={}".format(video_id)
+
+
+class FrontedURL:
+    def __init__(self,url,frontingdomain="drive.google.com"):
+        self.originalurl = url
+        parsed_url = up.urlparse(url)
+        s = parsed_url.scheme
+        d = parsed_url.netloc
+        p = parsed_url.path
+        q = parsed_url.query
+        new_url = s + "://" + frontingdomain + p + "?" + q
+        self.url = new_url
+        self.headers["Host"] = d
+    originalurl = ""
+    url = ""
+    headers = {}
+
+def openFrontedURL(fronted_url):
+    global globalheaders
+    headers = fronted_url.headers
+    headers.update(globalheaders)
+    req = ur.Request(fronted_url.url,headers=headers)
+    res = ur.urlopen(req)
+    return res
+
+
+video_url = input("Enter video url: ")
+
+video_id = getvideoid(video_url)
+meta_url = getmetaurl(video_id)
+
+
+fronted_url = FrontedURL(meta_url)
+res = openFrontedURL(fronted_url)
+
 b = res.read()
 s = b.decode("utf-8")
 
 
-class PageParser(html.parser.HTMLParser):
-    pass
-
-
-#ALL THE URLS IT SPITS BACK GIVE AN ERROR 403, WILL NEED TO FIX
-
 j = up.parse_qs(s)
 j2 = up.parse_qs(j["url_encoded_fmt_stream_map"][0])
-print(j2["url"])
 
-req2 = ur.Request(j2["url"][2],headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0","Origin":"https://www.youtube.com","Referer":"https://www.youtube.com/watch?v=_n56KPJVUQ8"})
+print(j2["url"])
+print(j2["quality"])
+print(j2["type"])
+
+
+
+#AUTOMATICALLY USES FIRST URL
+req2 = ur.Request(j2["url"][0],headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0","Origin":"https://www.youtube.com","Referer":video_url})
 
 res2 = ur.urlopen(req2)
+
+filename = savedir_full+"/youtubedltmp"
+
+CHUNK = 16*1024
+with open(filename,"wb") as f:
+    while True:
+        chunk = res2.read(CHUNK)
+        if not chunk: break
+        f.write(chunk)
+
+
 
