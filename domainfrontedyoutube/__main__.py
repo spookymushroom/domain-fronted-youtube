152c8a6e822f839aadcc1e11a739e17ef7d8f199
spookymushroom <github.spookymushroom@gmail.com>
2017-05-21 23:05:34 -0700

    added sanity checks for search and download

--- domainfrontedyoutube/__main__.py
+++ domainfrontedyoutube/__main__.py
@@ -14,22 +14,27 @@ while True:
     if u == "q": sys.exit(0)
     elif u == "h":
         print("Commands: ")
-        print("\tq: quit\n\th: display this help menu\n\tl: license information\n\ts <query>: search youtube\n\td <yt-link>: download youtube video to ~/Videos/youtubedltmp\n\tdebugoff: turn off verbose debug\n\tdebugon: turn on verbose debug")
+        print("\tq: quit\n\th: display this help menu\n\tl: license information\n\ts <query>: search youtube\n\td <yt-link>: download youtube video to ~/Videos/youtubedltmp\n\tdebugoff: turn off verbose debug\n\tdebugon: turn on verbose debug\n\tld: run the interactive legacy downloader")
     elif u == "l":
         print("This program is licensed under GNU General Public License v3. \
 More information can be found in the LICENSE file located in this program's root directory or at https://www.gnu.org/licenses/gpl-3.0.en.html")
     elif u.partition(" ")[0] == "s":
         q = u.partition(" ")[2]
-        results = __init__.searchyoutube(q)
-        for r in results:
-            if r["is_video"]: 
-                print(r["title"] + " (https://www.youtube.com" + r["url"] + ")")
-                print(" by " + r["channel_name"] + " (https://www.youtube.com" + r["channel_url"] + ")\n")
+        if q:
+            results = __init__.searchyoutube(q)
+            for r in results:
+                if r["is_video"]:
+                    print(r["title"] + " (https://www.youtube.com" + r["url"] + ")")
+                    print(" by " + r["channel_name"] + " (https://www.youtube.com" + r["channel_url"] + ")\n")
+        else: print("Usage: s <query>")
     elif u.partition(" ")[0] == "d":
         link = u.partition(" ")[2]
-        __init__.downloadfromlink(link)
+        if link: __init__.downloadfromlink(link)
+        else: print("Usage: d <youtube-link>")
     elif u == "debugoff":
         __init__.debug = False
     elif u == "debugon":
         __init__.debug = True
+    elif u == "ld":
+        __init__.legacydownloader()
 
